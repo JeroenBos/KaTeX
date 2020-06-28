8c7f94737f8abc8024fb90a53a76f7c7262571a3
Jeroen Bos <jeroen@deepinsight.io>
2020-06-28 15:01:39 +0200

    Settings

--- webpack.common.js
+++ webpack.common.js
@@ -24,31 +24,31 @@ type Target = {|
  */
 const targets /*: Array<Target> */ = [
     {
-        name: 'katex',
+        name: 'blatex',
         entry: './katex.webpack.js',
         library: 'katex',
     },
-    {
-        name: 'contrib/auto-render',
-        entry: './contrib/auto-render/auto-render.js',
-        library: 'renderMathInElement',
-    },
-    {
-        name: 'contrib/mhchem',
-        entry: './contrib/mhchem/mhchem.js',
-    },
-    {
-        name: 'contrib/copy-tex',
-        entry: './contrib/copy-tex/copy-tex.webpack.js',
-    },
-    {
-        name: 'contrib/mathtex-script-type',
-        entry: './contrib/mathtex-script-type/mathtex-script-type.js',
-    },
-    {
-        name: 'contrib/render-a11y-string',
-        entry: './contrib/render-a11y-string/render-a11y-string.js',
-    },
+    // {
+    //     name: 'contrib/auto-render',
+    //     entry: './contrib/auto-render/auto-render.js',
+    //     library: 'renderMathInElement',
+    // },
+    // {
+    //     name: 'contrib/mhchem',
+    //     entry: './contrib/mhchem/mhchem.js',
+    // },
+    // {
+    //     name: 'contrib/copy-tex',
+    //     entry: './contrib/copy-tex/copy-tex.webpack.js',
+    // },
+    // {
+    //     name: 'contrib/mathtex-script-type',
+    //     entry: './contrib/mathtex-script-type/mathtex-script-type.js',
+    // },
+    // {
+    //     name: 'contrib/render-a11y-string',
+    //     entry: './contrib/render-a11y-string/render-a11y-string.js',
+    // },
 ];
 
 /**
